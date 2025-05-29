75f972b3dcbe850d082f05e0f2352d0d0425bd46
Ridham <ridhammakvana99@gmail.com>
2025-05-29 16:03:52 +0530

    29

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -6,7 +6,7 @@ const HeroBanner = () => {
         style={{backgroundImage:`url(https://variety.com/wp-content/uploads/2023/10/jenna-ortega.jpg?w=1002)`,backgroundPosition:`center`}}>
         <div className='absolute inset-0 bg-black md:rounded-2xl bg-opacity-40 flex items-center justify-center'>
           <div className='text-center text-gray-100 px-4'>
-                   <h1 className='text-3xl md:text-5xl lg:text-6xl font-bold mb-4' >Discover Your Style</h1>
+                   <h1 className='text-3xl md:text-5xl lg:text-6xl sm:mb-10 font-bold mb-4' >Discover Your Style</h1>
                    <p className='text-lg md:text-xl mb-6'>Shop the latest and find your perfect look</p>
                    <button className='bg-transparant border border-white hover:bg-white hover:text-black text-white font-semibold py-2 px-4 md:py-3 md:px-6
                    rounded-lg transition duration-300' >Shop Now</button>
