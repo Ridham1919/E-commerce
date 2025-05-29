38202f23001132ac8bad538be76dc4d9de02c43d
Ridham <ridhammakvana99@gmail.com>
2025-05-29 14:33:07 +0530

    29

--- src/components/MidBanner.jsx
+++ src/components/MidBanner.jsx
@@ -3,7 +3,7 @@ const MidBanner = () => {
   return (
      <div className='bg-gray-100 py-24'>
        <div className='relative  max-w-6xl mx-auto md:rounded-2xl pt-28 bg-cover bg-center h-[500px] '
-        style={{backgroundImage:`url(https://assets.teenvogue.com/photos/64403f8809e18833cf1d7ab2/16:9/w_5999,h_3374,c_limit/1471768314)`,backgroundPosition:`120px `,backgroundAttachment:`fixed` }}>
+        style={{backgroundImage:`url(https://assets.teenvogue.com/photos/64403f8809e18833cf1d7ab2/16:9/w_5999,h_3374,c_limit/1471768314)`,backgroundPosition:`top `,backgroundAttachment:`fixed` }}>
         <div className='absolute inset-0 bg-black md:rounded-2xl bg-opacity-50 flex items-center justify-center'>
           <div className='text-center text-white px-4'>
                    <h1 className='text-3xl md:text-5xl lg:text-5xl font-bold mb-4'> Winter Collection 2025 </h1>
