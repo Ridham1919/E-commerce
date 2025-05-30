970a47c874bf157fb023d830478564b1f599b82d
Ridham <ridhammakvana99@gmail.com>
2025-05-30 15:53:06 +0530

    30

--- src/Pages/Womens.jsx
+++ src/Pages/Womens.jsx
@@ -10,12 +10,11 @@ const Womens = () => {
   return (
     <div className='md:mt-32 mt-20 max-w-7xl mx-auto'>
       <div >
-        <img src={banner} alt="" className='w-screen px-10 sm:px-5' />
+        <img src={banner} alt="" className='w-screen px-10 sm:px-7 sm:py-7' />
       </div>
-      <div className='mx-auto max-w-2xl px-4 py-16 sm:pt-24 lg:max-w-7xl lg:px-8'>
-        <h2 className='text-2xl font-bold tracking-tight text-gray-900 text-center md:text-start'>Women's Collection</h2>
+       <div className='mx-auto max-w-2xl px-4 py-16 sm:pt-24 lg:max-w-7xl lg:px-8'>
+        <h2 className='text-2xl font-bold tracking-tight text-gray-900 text-center md:text-start'>Men's Collection</h2>
         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 px-6 md:px-0 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-
           {
             WomenProducts.map((product) => {
               return <Item key={product.id} product={product} />
