e1b47261cd7ffb43f824b4ee998d95621e9a1438
Ridham <ridhammakvana99@gmail.com>
2025-05-30 17:08:18 +0530

    30

--- src/components/ResponsiveMenu.jsx
+++ src/components/ResponsiveMenu.jsx
@@ -27,7 +27,7 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
       ref={menuRef}
       className={`${showMenu ? "left-0" : "-left-[100%]"} fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
     >
-      <div>
+      <div className='h-[full]'>
         <div className="flex items-center justify-start gap-3">
           <FaUserCircle size={50} />
           <div>
