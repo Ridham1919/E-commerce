ecd1fd3d47f791ecda49b9d20860a1a6725c4aea
Ridham <ridhammakvana99@gmail.com>
2025-05-30 15:45:29 +0530

    30

--- src/Pages/Kids.jsx
+++ src/Pages/Kids.jsx
@@ -1,27 +1,27 @@
 import React, { useContext } from 'react'
-import {Shopcontext} from '../Context/ShopContext'
+import banner from '../assets/kids.png'
+import { Shopcontext } from '../Context/ShopContext'
 import Item from '../components/Item'
 
 
 const Kids = () => {
-  const {All_product} = useContext(Shopcontext)
-  const kidProducts = All_product.filter((product) => product.category ==="kid")
+  const { All_product } = useContext(Shopcontext)
+  const kidProducts = All_product.filter((product) => product.category === "kid")
   return (
     <div className='md:mt-32 mt-20 max-w-7xl mx-auto'>
       <div >
-         <img src="https://clothify-two.vercel.app/assets/KidsBanner-Bu4nYWZf.png" alt="" className='w-screen px-10' />
+        <img src={banner} alt="" className='w-screen px-10' />
       </div>
-          <div className='mx-auto max-w-2xl px-4 py-16 sm:pt-24 lg:max-w-7xl lg:px-8'>
-      <h2 className='text-2xl font-bold tracking-tight text-gray-900 text-center '>Kid's Collection</h2>
-      <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 px-6 md:px-0 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-
+      <div className='mx-auto max-w-2xl px-4 py-16 sm:pt-24 lg:max-w-7xl lg:px-8'>
+        <h2 className='text-2xl font-bold tracking-tight text-gray-900 text-center md:text-start'>Kid's Collection</h2>
+        <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 px-6 md:px-0 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
           {
-            kidProducts.map((product)=>{
-              return <Item key={product.id} product={product}/>
+            kidProducts.map((product) => {
+              return <Item key={product.id} product={product} />
             })
 
           }
-         </div>
+        </div>
       </div>
     </div>
   )
