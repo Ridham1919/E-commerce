e367ae4a4aaf2eb5a06024a078cb988a7a9ee2be
Ridham <ridhammakvana99@gmail.com>
2025-05-30 15:26:39 +0530

    30

--- src/Pages/Mens.jsx
+++ src/Pages/Mens.jsx
@@ -11,9 +11,10 @@ const Mens = () => {
       <div >
          <img src="https://clothify-two.vercel.app/assets/MensBanner-GCNHMmGk.png" alt="" className='w-screen ' />
       </div>
-      <div className='mx-auto max-w-2xl sm:mb-24  sm:pt-12 lg:max-w-7xl lg:px-8'>
-         <h2 className='text-3xl font-bold tracking-tight text-gray-900 text-center pb-6 '>Mens's Collection</h2>
-         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 px-6 md:px-0 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
+           <div className='mx-auto max-w-2xl px-4 py-16 sm:pt-24 lg:max-w-7xl lg:px-8'>
+        <h2 className='text-2xl font-bold tracking-tight text-gray-900 text-center '>Men's Collection</h2>
+        <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 px-6 md:px-0 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
+
           {
             menProducts.map((product)=>{
               return <Item key={product.id} product={product}/>
