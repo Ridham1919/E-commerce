b5a606c0ccfabb394949786a69cffba570514921
Ridham <ridhammakvana99@gmail.com>
2025-06-01 17:49:14 +0530

    1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,39 +13,39 @@ const Navbar = () => {
     const toggleMenu = () => {
         setShowMenu(!showMenu)
     }
-  return (
-       <div className='bg-white px-4  fixed w-full md:h-[80px] z-50 shadow-sm top-0 shadow-gray-400'>
-           <div className='max-w-7xl mx-auto py-2 px-5 flex justify-between items-center'>
-            <div className=''>
-               <img src={Logo} alt="" className=' sm:w-5  md:mt-3 md:w-[100px] w-20 '/>
-            </div>
-               <div className='flex mt-3 items-center gap-5'>
-                   <nav className='hidden md:block'>
-                       <ul className='flex items-center font-semibold text-base gap-7'>
-                           <Link to={'/'}><li>Home</li></Link>
-                           <Link to={'/mens'}>Mens</Link>
-                           <Link to={'/womens'}> <li>Womens</li></Link>
-                           <Link to={'/kids'}><li>Kids</li></Link>
-                           <Link to={'/login'}> <button className='bg-red-500 text-white px-4 py-2 rounded-md'>Login</button></Link>
-                       </ul>
-                   </nav>
-                   <Link to={'/cart'} className='relative w-10'>
-                   <FiShoppingCart size={25} />
-                   <div className='bg-red-500 w-4 h-4 text-xs absolute -top-2 right-1 flex items-center justify-center rounded-full
+    return (
+        <div className='bg-white px-4  fixed w-full md:h-[80px] z-50 shadow-sm top-0 shadow-gray-400'>
+            <div className='max-w-7xl mx-auto py-2 px-5 flex justify-between items-center'>
+                <div className=''>
+                    <img src={Logo} alt="" className=' sm:w-5  md:mt-3 md:w-[100px] w-20 ' />
+                </div>
+                <div className='flex mt-3 items-center gap-5'>
+                    <nav className='hidden md:block'>
+                        <ul className='flex items-center font-semibold text-base gap-7'>
+                            <Link to={'/'}><li>Home</li></Link>
+                            <Link to={'/mens'}>Mens</Link>
+                            <Link to={'/womens'}> <li>Womens</li></Link>
+                            <Link to={'/kids'}><li>Kids</li></Link>
+                            <Link to={'/login'}> <button className='bg-red-500 text-white px-4 py-2 rounded-md'>Login</button></Link>
+                        </ul>
+                    </nav>
+                    <Link to={'/cart'} className='relative w-10'>
+                        <FiShoppingCart size={25} />
+                        <div className='bg-red-500 w-4 h-4 text-xs absolute -top-2 right-1 flex items-center justify-center rounded-full
                    text-white'>0</div>
-                   </Link>
+                    </Link>
 
-                   {/* mobile hambure icons */}
-                   {showMenu ? (
-                     <HiMenuAlt1 onClick={toggleMenu} className='cursor-pointer transition-all md:hidden' size={30} />
-                   ):(
-                   <HiMenuAlt3 onClick={toggleMenu} className='cursor-pointer transition-all md:hidden' size={30} />
-                   )}
-               </div>
-           </div>
-           <ResponsiveMenu showMenu={showMenu} setShowMenu={setShowMenu}/>
-       </div>
-  )
+                    {/* mobile hambure icons */}
+                    {showMenu ? (
+                        <HiMenuAlt1 onClick={toggleMenu} className='cursor-pointer pb-3 transition-all md:hidden' size={30} />
+                    ) : (
+                        <HiMenuAlt3 onClick={toggleMenu} className='cursor-pointer pb-3 transition-all md:hidden' size={30} />
+                    )}
+                </div>
+            </div>
+            <ResponsiveMenu showMenu={showMenu} setShowMenu={setShowMenu} />
+        </div>
+    )
 }
 
 export default Navbar
