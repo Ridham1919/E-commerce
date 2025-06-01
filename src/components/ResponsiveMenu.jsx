dc12be38fe62153d39c9a5e106b0ac40e3a88d01
Ridham <ridhammakvana99@gmail.com>
2025-06-01 10:41:10 +0530

    1

--- src/components/ResponsiveMenu.jsx
+++ src/components/ResponsiveMenu.jsx
@@ -25,10 +25,10 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
   return (
     <div
       ref={menuRef}
-      className={`${showMenu ? "left-0" : "-left-[100%]"} fixed  z-20 flex h-[100%] w-[75%] flex-col justify-between bg-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
+      className={`${showMenu ? "left-0" : "-left-[100%]"} fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white px-8 pb-6 pt-16 text-black md:hidden rounded-r-xl shadow-md`}
     >
       <div className=''>
-        <div className="flex items-center justify-start gap-3">
+        <div className='flex items-center justify-start gap-3'>
           <FaUserCircle size={50} />
           <div>
             <h1>Hello User</h1>
