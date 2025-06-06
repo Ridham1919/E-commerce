739ed2a03158f7e699802e5b44df4beeb491c0e9
Ridham <ridhammakvana99@gmail.com>
2025-06-06 15:22:59 +0530

    6-june

--- src/components/MidBanner.jsx
+++ src/components/MidBanner.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+
+
 const MidBanner = () => {
   return (
-     <div className='bg-gray-100 py-10'>
-       <div className='relative  max-w-6xl mx-auto md:rounded-2xl pt-28 bg-cover bg-center h-[500px] '
-        style={{backgroundImage:`url(https://assets.teenvogue.com/photos/64403f8809e18833cf1d7ab2/16:9/w_5999,h_3374,c_limit/1471768314)`,backgroundAttachment:`fixed` }}>
+    <div className='bg-gray-100 py-10'>
+      <div className='relative  max-w-6xl mx-auto md:rounded-2xl pt-28 bg-cover bg-center h-[500px] '
+        style={{ backgroundImage: `url(https://assets.teenvogue.com/photos/64403f8809e18833cf1d7ab2/16:9/w_5999,h_3374,c_limit/1471768314)`, backgroundAttachment: `fixed` }}>
         <div className='absolute inset-0 bg-black md:rounded-2xl bg-opacity-50 flex items-center justify-center'>
           <div className='text-center text-white px-4'>
-                   <h1 className='text-3xl md:text-5xl lg:text-5xl font-bold mb-4'> Winter Collection 2025 </h1>
-                   <p className='text-lg md:text-xl mb-6'>Shop the latest and find your perfect look</p>
-<button
+            <h1 className='text-3xl md:text-5xl lg:text-5xl font-bold mb-4'> Winter Collection 2025 </h1>
+            <p className='text-lg md:text-xl mb-6'>Shop the latest and find your perfect look</p>
+            <button
               onClick={() => {
                 const section = document.getElementById("TopSeller");
                 section?.scrollIntoView({ behavior: "smooth" });
@@ -19,7 +21,7 @@ const MidBanner = () => {
             </button>
           </div>
         </div>
-       </div>
+      </div>
     </div>
   )
 }
